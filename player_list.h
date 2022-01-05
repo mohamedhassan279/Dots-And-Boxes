93da74780e4d9538ed762f372e4ca915b65fdf11
Mohamed <mo.hassan2792@gmail.com>
2022-01-05 15:49:01 +0200

    Adding players list

---
+++ player_list.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_LIST_H_INCLUDED
+#define PLAYER_LIST_H_INCLUDED
+
+typedef struct
+{
+	int name_len;
+	char name[MAX];
+	int score;
+
+}gamer;
+
+gamer winners_list[MAX] = {0};
+
+#endif // PLAYER_LIST_H_INCLUDED
